7dc97bdad18adca31eb52367d0c0913659434b09
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-08 00:36:02 +0700

    Fix Submission 1 9 on 9

--- Submission 1/07-1-handling-error/app.js
+++ Submission 1/07-1-handling-error/app.js
@@ -1,3 +1,15 @@
 function parseURL(url) {
-  return new URL(url);
+  try {
+    return new URL(url);
+  } catch (error) {
+    return null
+  }
+}
+
+// example case
+const parsedUrl = parseURL("https://www.dand.com")
+if (parsedUrl !== null) {
+  console.log(parsedUrl); 
+} else {
+  console.log("URL tidak valid.");
 }
