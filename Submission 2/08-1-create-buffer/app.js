d8148575281fb8f6316932d0eaed933cc026f327
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-08 10:40:55 +0700

    Fix Submission 2 (1/11)

--- Submission 2/08-1-create-buffer/app.js
+++ Submission 2/08-1-create-buffer/app.js
@@ -5,3 +5,16 @@
  * 2. Bernama `safeBuffer` dan buatlah dengan cara safe dengan alokasi memori 100000 bytes.
  * 3. Bernama `stringBuffer` dan buatlah dari nilai string "Dicoding".
  */
+// Membuat buffer dengan cara unsafe dengan alokasi memori 100000 bytes
+const unsafeBuffer = Buffer.allocUnsafe(100000);
+
+// Membuat buffer dengan cara safe dengan alokasi memori 100000 bytes
+const safeBuffer = Buffer.alloc(100000);
+
+// Membuat buffer dari nilai string "Dicoding"
+const stringBuffer = Buffer.from('Dicoding');
+
+// Untuk melihat buffer yang telah dibuat
+console.log('unsafeBuffer:', unsafeBuffer);
+console.log('safeBuffer:', safeBuffer);
+console.log('stringBuffer:', stringBuffer);
